f6e0733dac27ea27ffdde660ab027578c506a1ae
dimbodat <dimaskoswara27@gmail.com>
2024-08-28 23:58:46 +0700

    belajarTypescript: task if-else selesai 100%

--- if-else/task.js
+++ if-else/task.js
@@ -23,4 +23,66 @@
  *          var hari = 21; var bulan = 1; var tahun = 1945;
  *          Maka hasil yang akan tampil di console adalah: '21 Januari 1945';
  **/
-/*  kerjakan tugas di bawah ini */ 
+/*  kerjakan tugas di bawah ini */
+console.log("================= soal 1 =================");
+const nama = "Max";
+const alamat = "New York";
+const umur = 30;
+const jenisKelamin = "male";
+const userLogin = false;
+let description = "hello, my name is " + nama + ". i live in " + alamat + " i am " + jenisKelamin + "," + umur + " years ";
+if (userLogin) {
+    console.log("Max is loggedIn");
+}
+else {
+    console.log("Max is not loggedIn");
+}
+console.log(description);
+console.log("================= soal 2 =================");
+const day = 1;
+const month = 5;
+const year = 1945;
+let monthName;
+switch (month) {
+    case 1:
+        monthName = "Januari";
+        break;
+    case 2:
+        monthName = "Februari";
+        break;
+    case 3:
+        monthName = "Maret";
+        break;
+    case 4:
+        monthName = "April";
+        break;
+    case 5:
+        monthName = "Mei";
+        break;
+    case 6:
+        monthName = "Juni";
+        break;
+    case 7:
+        monthName = "Juli";
+        break;
+    case 8:
+        monthName = "Agustus";
+        break;
+    case 9:
+        monthName = "September";
+        break;
+    case 10:
+        monthName = "Oktober";
+        break;
+    case 11:
+        monthName = "November";
+        break;
+    case 12:
+        monthName = "Desember";
+        break;
+    default:
+        monthName = "Tidak valid";
+        break;
+}
+const formatDate = +day + " " + monthName + " " + year;
+console.log(formatDate);
