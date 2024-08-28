159d4ab71929c29be7ec7353e5b7f5b3093daffa
dimbodat <dimaskoswara27@gmail.com>
2024-08-28 19:16:28 +0700

    belajarTypescript: update if else

---
+++ if-else/task.js
@@ -0,0 +1,26 @@
+"use strict";
+/**
+ * @module if-else/task
+ *
+ * @description
+ * - This module is used to learn about if-else in TypeScript.
+ * - This module consists of two if-elses, and switch-case.
+ * - tugas:
+ *     1. buat nama variabel dengan contoh
+ *        - nama: "Max"
+ *        - alamat: "New York"
+ *        - umur: 30
+ *        - jenis kelamin: "laki-laki"
+ *        - userLogin: false
+ *
+ *        hasilnya: "Hello, my name is Max. I live in New York. Iam male, 30 years."
+ *        jika userLogin bernilai true, maka tambahkan "notes: Max is loggedIn"
+ *        jika userLogin bernilai false, maka tambahkan "notes: Max is not loggedIn"
+ *
+ *      2. Kamu akan diberikan sebuah tanggal dalam tiga variabel, yaitu hari, bulan, dan tahun. Disini kamu diminta untuk membuat format tanggal. Misal tanggal yang diberikan adalah hari 1, bulan 5, dan tahun 1945. Maka, output yang harus kamu proses adalah menjadi 1 Mei 1945.
+ *         Gunakan switch-case untuk kasus ini!
+ *         Contoh:
+ *          var hari = 21; var bulan = 1; var tahun = 1945;
+ *          Maka hasil yang akan tampil di console adalah: '21 Januari 1945';
+ **/
+/*  kerjakan tugas di bawah ini */ 
