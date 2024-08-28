8323f5c3147bba9930ecb1cf49ce6d8cb2e0b2b8
dimbodat <dimaskoswara27@gmail.com>
2024-08-28 19:11:27 +0700

    belajarTypescript: update loop js and ts, task js and ts

---
+++ loop/loop.ts
@@ -0,0 +1,31 @@
+/**
+ * loop
+ */
+
+
+// const initialNumber: number = 0;
+// const finalNumber: number = 10;
+// const increment: number = 1;
+
+// // for (let i = initialNumber; i < finalNumber; i = i + increment) {
+// //     console.log(i + 2);
+// // }
+
+// for (let i = initialNumber; i < finalNumber; i++) {
+//     let star = "";
+//     for (let j = 0; j <= i; j++) {
+//         star += "*";
+//     }
+
+//     console.log(star);
+// }
+
+
+// loop while
+
+let intialNumberWhile: number = 0;
+
+while (intialNumberWhile < 10) {
+    console.log(intialNumberWhile);
+    intialNumberWhile++;
+}
---
+++ loop/task.ts
@@ -0,0 +1,67 @@
+// buatlah loop terbalik, dari atas kebawah 100 ke 0
+
+// const initialNumber: number = 100;
+// const finalNumber: number = 0;
+
+
+// for (let i = initialNumber; i >= finalNumber; i--) {
+//         console.log(i);
+//     }
+
+
+// buatlah loop dari 1 - 100, tampilkan hanya angka ganjil
+
+// const initialNumber: number = 1;
+// const finalNumber: number = 100;
+// const increment: number = 1;
+
+// for (let i = initialNumber; i <= finalNumber; i += increment) {
+//     if (i % 2 !== 0) {
+//       console.log(i);
+//     }
+//   }
+  
+
+// buatlah loop dari 1 - 100, tampilkan hany angka genap
+
+// const initialNumber: number = 2;
+// const finalNumber: number = 100;
+// const increment: number = 1;
+
+// for(let i = initialNumber; i <= finalNumber; i += increment) {
+//     if (i % 2 === 0) {
+//         console.log(i);
+//     }
+// }
+
+// main bintang
+
+// const initialNumber: number = 0;
+// const finalNumber: number = 100;
+// const increment: number = 1;
+
+// for (let i = initialNumber; i < finalNumber; i++) {
+//     let star = "";
+//     for (let j = 0; j <= i; j++) {
+//         star += "*";
+//     }
+
+//     console.log(star);
+// }
+
+/**
+ * @module loop/task
+ * 
+ * @description
+ * - this module is user to learn about loop in TypeScript
+ * - this module consists of two loops, for and while.
+ * - tugas
+ *     - loop for terbalik (reverse for loop)
+ *     - loop for dengan kondisi tertentu (for loop with certain condition)
+ *     - loop asterisk
+ *     - loop while terbalik (reverse while loop)
+ *     - loop while dengan kondisi tertentu (while loop with certain condition)
+ *     - loop while asterisk
+ */
+
+/*  kerjakan tugas di bawah ini */
